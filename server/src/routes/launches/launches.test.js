4cf3e79843491ac5bd71f696ba1dafb3ae5733f7
Srutip04 <subuhishruti75@gmail.com>
2022-06-27 21:41:30 +0530

    erroe cases

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,50 +1,70 @@
-const request = require('supertest');
-const app = require('../../app');
+const request = require("supertest");
+const app = require("../../app");
 
-
-describe('Test GET /launches', () => {
-    test('should respond with 200 success',async () =>{
-        const response = await request(app)
-        .get('/launches')
-        .expect('Content-Type', /json/)
-        .expect(200);
-        
-    })
+describe("Test GET /launches", () => {
+  test("should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
 });
 
-describe('Test POST /launches', () => {
-    const completeLaunchData = {
-      mission: "uss enterprise",
-      rocket: "falcon 9",
-      target: "Kepler-186 f",
-      launchDate: "Januray 1, 2028",
-    };
-    
-    const launchDataWithoutDate = {
-      mission: "uss enterprise",
-      rocket: "falcon 9",
-      target: "Kepler-186 f",
-   
-    };
+describe("Test POST /launches", () => {
+  const completeLaunchData = {
+    mission: "uss enterprise",
+    rocket: "falcon 9",
+    target: "Kepler-186 f",
+    launchDate: "Januray 1, 2028",
+  };
 
-    test('should respond with 201 created',async () =>{
-       const response = await request(app)
-         .post("/launches")
-         .send(completeLaunchData)
-         .expect("Content-Type", /json/)
-         .expect(201);
+  const launchDataWithoutDate = {
+    mission: "uss enterprise",
+    rocket: "falcon 9",
+    target: "Kepler-186 f",
+  };
 
-         const requestDate = new Date(completeLaunchData.launchDate).valueOf();
-         const responseDate = new Date(response.body.launchDate).valueOf();
-         expect(responseDate).toBe(requestDate);
+  const launchDataWithInvalidDate = {
+    mission: "uss enterprise",
+    rocket: "falcon 9",
+    target: "Kepler-186 f",
+    launchDate: "Zotoo",
+  };
+ 
+  test("should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
 
-         expect(response.body).toMatchObject(launchDataWithoutDate);
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  test("it should catch missing reqd fields", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
     });
+  });
+  test("it should catch invalid date",async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
 
-    test('it should catch missing reqd fields',() =>{
-       
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date",
     });
-    test('it should catch invalid date',() =>{
-       
-    });
-})
+  });
+});
