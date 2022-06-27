d1d74dce4b290f3ec3cfa95b2a3dfa51273d9c89
Srutip04 <subuhishruti75@gmail.com>
2022-06-27 21:09:35 +0530

    testing APIs with jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,19 @@
+describe('Test GET /launches', () => {
+    test('should respond with 200 success',() =>{
+        const response = 200;
+        expect(response).toBe(200);
+    })
+});
+
+describe('Test POST /launches', () => {
+    test('should respond with 200 success',() =>{
+       
+    });
+
+    test('it should catch missing reqd fields',() =>{
+       
+    });
+    test('it should catch invalid date',() =>{
+       
+    });
+})
