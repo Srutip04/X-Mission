fe475944ab0672873e40850c1102e018cdf1c3a8
Srutip04 <subuhishruti75@gmail.com>
2022-06-27 21:33:52 +0530

    POST

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -13,8 +13,32 @@ describe('Test GET /launches', () => {
 });
 
 describe('Test POST /launches', () => {
-    test('should respond with 200 success',() =>{
-       
+    const completeLaunchData = {
+      mission: "uss enterprise",
+      rocket: "falcon 9",
+      target: "Kepler-186 f",
+      launchDate: "Januray 1, 2028",
+    };
+    
+    const launchDataWithoutDate = {
+      mission: "uss enterprise",
+      rocket: "falcon 9",
+      target: "Kepler-186 f",
+   
+    };
+
+    test('should respond with 201 created',async () =>{
+       const response = await request(app)
+         .post("/launches")
+         .send(completeLaunchData)
+         .expect("Content-Type", /json/)
+         .expect(201);
+
+         const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+         const responseDate = new Date(response.body.launchDate).valueOf();
+         expect(responseDate).toBe(requestDate);
+
+         expect(response.body).toMatchObject(launchDataWithoutDate);
     });
 
     test('it should catch missing reqd fields',() =>{
