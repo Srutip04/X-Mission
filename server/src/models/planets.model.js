6b5870625c5e8574b698bc676ef68257bf58a4d1
Srutip04 <subuhishruti75@gmail.com>
2022-06-27 18:17:13 +0530

    data access layer

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -47,8 +47,11 @@ function loadPlanetsData(){
     })
 }
 
+function getAllPlanets(){
+    return habitablePlanets;
+}
 
 module.exports = {
   loadPlanetsData,
-  planets: habitablePlanets,
+  getAllPlanets,
 }; 
