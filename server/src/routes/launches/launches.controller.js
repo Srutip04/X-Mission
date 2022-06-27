4a4393d6a2d9e5dba17a58057fe56e1038a74b92
Srutip04 <subuhishruti75@gmail.com>
2022-06-27 20:18:18 +0530

    aborting launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const {getAllLaunches,addNewLaunch} = require('../../models/launches.model');
+const {getAllLaunches,addNewLaunch,existsLaunchWithId} = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
  return  res.status(200).json(getAllLaunches());
@@ -24,4 +24,18 @@ function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch);
 }
 
-module.exports = { httpGetAllLaunches, httpAddNewLaunch };
+function httpAbortLaunch(req, res) {
+  const launchId = req.params.id;
+//   const launch = getAllLaunches().find(l => l.flightNumber === launchId);
+  if(!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+        error: "Launch not found"
+    })
+  }
+
+//   launch.upcoming = false;
+//   launch.success = false;
+  return res.status(200).json(aborted);
+}
+
+module.exports = { httpGetAllLaunches, httpAddNewLaunch , httpAbortLaunch };
