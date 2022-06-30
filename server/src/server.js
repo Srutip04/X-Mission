faffdbe7c3745dcb1108a9d8c28f1409db1615f5
Srutip04 <subuhishruti75@gmail.com>
2022-06-30 20:36:44 +0530

    loading SpaceX data

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 
 const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
 const app = require("./app");
 
 const {mongoConnect} = require('./services/mongo');
@@ -15,6 +16,7 @@ const server = http.createServer(app);
 async function startServer(){
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
   
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
