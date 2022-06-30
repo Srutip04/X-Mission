77a989b7facc023ba6eadb2c5fa59f9555d0b9e0
Srutip04 <subuhishruti75@gmail.com>
2022-06-30 20:53:31 +0530

    mapping SpaceX data to database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -20,7 +20,7 @@ saveLaunch(launch);
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
-async function loadLaunchesData(){
+async function loadLaunchData(){
   console.log('Loading launches data...');
  const response =  await axios.post(SPACEX_API_URL, {
     query: {},
@@ -41,6 +41,24 @@ async function loadLaunchesData(){
       ],
     },
   });
+  const launchDocs = response.data.docs;
+  for(const launchDoc of launchDocs){
+    const payloads = launchDoc["payloads"];
+    const customers = payloads.flatMap((payload) => {
+      return payload["customers"];
+    });
+
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      launchDate: launchDoc["date_local"],
+      upcoming: launchDoc["upcoming"],
+      success: launchDoc["success"],
+      customers,
+    };
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+  }
 }
 
 
@@ -97,6 +115,7 @@ return aborted.modifiedCount === 1;
 }
 
 module.exports = {
+    loadLaunchData,
     getAllLaunches,
     scheduleNewLaunch,
     existsLaunchWithId,
